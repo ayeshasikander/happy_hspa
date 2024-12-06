78ac33e45ec01f5c6aa5c5845cf9384586d84cbe
ayeshasikander <ayesha@724.one>
2024-12-06 18:04:35 +0500

    events

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
+'use client';
 export default function Home() {
+  const handleClick=()=>alert("hello i am alert in next js")
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      hello
-    </div>
+    <>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        hello
+        <button onClick={handleClick}>
+          click me
+        </button>
+      </div>
+    </>
   );
 }
